a0e4b1f120d1d76380ed21a202e84e8d07404c26
tiagonys <tiagonysdias@gmail.com>
2024-02-27 14:09:42 -0300

    add usuario2 no projeto SmartTv

---
+++ java-projetoSmartTv/sistema-smartv/src/Usuario2.java
@@ -0,0 +1,10 @@
+public class Usuario2 {
+    public static void main(String[] args) {  
+    
+    SmartTv smartTv2 = new SmartTv();
+    smartTv2.diminuirVolume();
+    System.out.println("teste: " +smartTv2.volume);
+
+    }
+
+}
