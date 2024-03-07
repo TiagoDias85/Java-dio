37a2f01b329a84431820f2b2a94d39cc51c84bac
tiagonys <tiagonysdias@gmail.com>
2024-03-07 17:27:13 -0300

    Adicionando o desafio Contador

--- Escopo/Desafio-Candidatos/src/Candidatura/ProcessoSeletivo.java
+++ Escopo/Desafio-Candidatos/src/Candidatura/ProcessoSeletivo.java
@@ -1,10 +1,43 @@
 package Candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		imprimirSelecionados();
+		String [] candidatosSelecionados = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+		//primeiro um for para selecionar os candidatos
+		for(String candidato: candidatosSelecionados) {
+			contatoEntrevista(candidato);
+		}
+	}
+	
+static void contatoEntrevista(String candidato) {
+		
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+		do {
+			atendeu= atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+		
+		
+	}
+	
+	//método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
 	}
 	
 	static void imprimirSelecionados () {
